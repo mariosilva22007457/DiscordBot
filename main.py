97e9e1f1ecf72532d57b672f19ea95e918555e2e
Mário Malta da Silva a22007457 <79096482+mariosilva22007457@users.noreply.github.com>
2022-09-06 17:26:36 +0100

    Debug Terminal Bot Info

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 
 from dotenv import load_dotenv
 import discord
+from colorama import Back,Fore,Style
+import time
+import platform
 from discord.ext import commands
 
 load_dotenv()
@@ -14,5 +17,12 @@ client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 async def hello(ctx):
     await ctx.send("Ola sou um bot chines ")
 
+@client.event
+async def on_ready():
+    prfx = (Back.LIGHTBLACK_EX+Fore.GREEN + time.strftime("%H:%M:%S UTC", time.gmtime()) + Back.RESET + Fore.WHITE+ Style.BRIGHT)
+    print(prfx + " Logged in as " + Fore.CYAN + client.user.name)
+    print(prfx + " Bot Id " + Fore.CYAN + str(client.user.id))
+    print(prfx + " Discord Version " + Fore.CYAN + discord.__version__)
+    print(prfx + " Python Version " + Fore.CYAN + str(platform.python_version()))
 
 client.run(TOKEN)
