342563fd15b0e5ae000e0ec6dd9095a05b7c430d
ricardocleto22006526 <a22006526@alunos.ulht.pt>
2022-09-10 18:35:25 +0100

    Changed Identation & Variable names

--- main.py
+++ main.py
@@ -24,11 +24,16 @@ async def hello(ctx):
     await ctx.send("Ola sou um bot chines ")
 
 
-
 @client.event
 async def on_ready():
-    prfx = (Back.LIGHTBLACK_EX + Fore.GREEN + time.strftime("%H:%M:%S UTC",
-                                                            time.gmtime()) + Back.RESET + Fore.WHITE + Style.BRIGHT)
+    prfx = (
+            Back.LIGHTBLACK_EX
+            + Fore.GREEN
+            + time.strftime("%H:%M:%S UTC", time.gmtime())
+            + Back.RESET
+            + Fore.WHITE
+            + Style.BRIGHT
+    )
     print(prfx + " Logged in as " + Fore.CYAN + client.user.name)
     print(prfx + " Bot Id " + Fore.CYAN + str(client.user.id))
     print(prfx + " Discord Version " + Fore.CYAN + discord.__version__)
@@ -43,23 +48,63 @@ async def shutdown(ctx):
 
 @client.command(aliases=['uinfo', 'whois'])
 async def userinfo(ctx, member: discord.Member = None):
+
     if member == None:
         member = ctx.message.author
-    roles = [role for role in member.roles]
-    embed = discord.Embed(title="User info", description=f"User info on the user {member.name}",
-                          color=discord.Color.green(), timestamp=ctx.message.created_at)
-    embed.set_thumbnail(url=member.avatar)
-    embed.add_field(name="ID", value=member.id)
-    embed.add_field(name="Name", value=f"{member.name}#{member.discriminator}")
-    embed.add_field(name="Nickname", value=member.display_name)
-    embed.add_field(name="Status", value=member.status)
-    embed.add_field(name="Created at ", value=member.created_at.strftime("%a, %B %#d, %Y, %I:%M %p "))
-    embed.add_field(name="Joined at ", value=member.joined_at.strftime("%a, %B %#d, %Y, %I:%M %p "))
-    embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
-    embed.add_field(name="Top Role", value=member.top_role.mention)
-    # embed.add_field(name="Messages", value="007")
-    embed.add_field(name="Bot?", value=member.bot)
-    await ctx.send(embed=embed)
+
+    discord_USER_ROLES = [role for role in member.roles]
+
+    bot_USER_OUTPUT_INFOS = discord.Embed(
+        title="User info", description=f"User info on the user {member.name}",
+        color=discord.Color.green(), timestamp=ctx.message.created_at
+    )
+
+    bot_USER_OUTPUT_INFOS.set_thumbnail(
+        url=member.avatar
+    )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="ID", value=member.id
+    )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="Name", value=f"{member.name}#{member.discriminator}"
+    )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="Nickname", value=member.display_name
+    )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="Status", value=member.status
+    )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="Created at ", value=member.created_at.strftime("%a, %B %#d, %Y, %I:%M %p ")
+    )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="Joined at ", value=member.joined_at.strftime("%a, %B %#d, %Y, %I:%M %p ")
+    )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name=f"Roles ({len(discord_USER_ROLES)})", value=" ".join([role.mention for role in discord_USER_ROLES])
+    )
+
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="Top Role", value=member.top_role.mention
+    )
+
+    # embed.add_field(
+    # name="Messages", value="007"
+    # )
+
+    bot_USER_OUTPUT_INFOS.add_field(
+        name="Bot?", value=member.bot
+    )
+
+    await ctx.send(embed=bot_USER_OUTPUT_INFOS)
 
 
 @client.event
@@ -80,8 +125,8 @@ async def on_member_join(member):
 
 @client.event
 async def on_member_remove(member):
-    channel = client.get_channel(837870956781109258)
-    await channel.send("Goodbye!" + f" {member.display_name} ")
+    channel_REMOVED_USER = client.get_channel(837870956781109258)
+    await channel_REMOVED_USER.send("Goodbye!" + f" {member.display_name} ")
 
 
 client.run(TOKEN)
